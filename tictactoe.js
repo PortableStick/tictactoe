ee397cfc71f5360479de04715b7682b4a48a42d5
Greg Smith <crashingwaves@fea.st>
2015-11-12 13:46:41 -0700

    Refactored with jQuery/Bootstrap; moved some code to new GameController; New modal windows working

--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,4 @@
-function gameboard(){
+function Gameboard(){
 	function _tile(x, y){
 		var canvas 			= document.createElement("canvas"),
 			context 		= canvas.getContext("2d");
@@ -66,35 +66,28 @@ function gameboard(){
 		context			: null,
 		tiles			: [],
 		init : function(){
-				var currentContext 	= this;
-				
-					this.context	= this.canvas.getContext("2d");
-					this.tiles		= (function(){
-							var tmpArray = [];
-							for(var i = 0; i < 9; i++){
-								var x = i % 3 * 140 + 20,
-									y = Math.floor(i/3) * 140 + 20;
-									tmpArray.push(_tile(x,y));
-									console.log(tmpArray[i]);
-							}
-							return tmpArray;
-						})();
-
-				this.reset();
-					
 				this.canvas.width = this.canvas.height = 440;
-
-				
-			this.tick();
+				this.context	= this.canvas.getContext("2d");
+				this.tiles		= (function(){
+						var tmpArray = [];
+						for(var i = 0; i < 9; i++){
+							var x = i % 3 * 140 + 20,
+								y = Math.floor(i/3) * 140 + 20;
+								tmpArray.push(_tile(x,y));
+						}
+						return tmpArray;
+					})();
+				this.resetBoard();									
+				document.body.appendChild(this.canvas);
+				this.tick();
 		},
-		setTile : function(tileIdx, newTile){
+		setTile: function(tileIdx, newTile){
 			this.tiles[tileIdx].setCurrentTile(newTile);
 		},
-		reset: function(){
+		resetBoard: function(){
 			this.tiles.map(function(x){
 				x.setCurrentTile("BLANK_tile");
-				});
-			this.currentPlayer = "X_tile";
+			});
 		},
 		draw : function(tile){
 			//console.log(tile.getCurrentTile());
@@ -115,14 +108,83 @@ function gameboard(){
 			for(var i = this.tiles.length;i--;){
 				this.draw(this.tiles[i]);
 			}
+		}
+	}
+}
+
+function aiPlayer(){
+	return {
+
+	}
+}
+
+function GameController(){
+	return{
+		aiOpponent: aiPlayer(),
+		gameboard: Gameboard(),
+		gameMode: null,
+		playerTile: null,
+		depth: 0,
+		currentPlayer: "X_tile",
+		init: function(){
+			$('#startModal').show();
+			$("#gameModeChoice").change(function(){
+				if($("#gameModeChoice option:selected")[0].value === "2P"){
+					$("#difficulty").hide();
+				} else {
+					$("#difficulty").show();
+				}
+			});
+			$('#startButton').click(function(){
+				$('#startModal').hide();
+				var options = $('#gameOptions').serializeArray();
+				controller.gameMode 	= options[0].value;
+				controller.depth		= options[1].value;
+				controller.playerTile 	= options[2].value;
+			});
+			this.gameboard.init();
+			$('#resetButton').click(this.resetGame.bind(this));
+			var board = this.gameboard,
+				controller	= this;
+				board.canvas.addEventListener('mousedown', function(){
+									var targetElement 			= event.target,
+										xPosition				= event.clientX - targetElement.offsetLeft,
+										yPosition				= event.clientY - targetElement.offsetTop;
+		
+										if(xPosition % 140 >= 20 && yPosition % 140 >= 20){
+											var idx = Math.floor(xPosition/140) + (Math.floor(yPosition/140) * 3);
+
+											board.setTile(idx, controller.currentPlayer);
+											var result = controller.evaluateBoard(board.tiles, controller.currentPlayer);
+
+											if(result){
+												controller.endGame(result);
+											}
+
+											if(controller.gameMode === "1P"){
+
+											} else if(controller.gameMode === "2P"){
+												controller.currentPlayer = controller.currentPlayer === "X_tile" ? "O_tile" : "X_tile";
+											}
+										}
+								});
+		},
+		resetGame: function(){
+			this.gameboard.resetBoard();
+			this.currentPlayer  = "X_tile";
+			$("#startModal").show();
+			$("#endModal").hide();
+		},
+		endGame: function(message){
+			$('#endModal').show();
+			$('#endMessage').html(message);
 		},
 		evaluateBoard: function(gameStateArray, currentPlayer){
 			var winningStates	= [	"111000000", "000111000", "000000111",
 									"100100100", "001001001", "010010010",
-									"001010100", "100010001" ]
-				.map(function(x){
-					return parseInt(x, 2);
-				}),
+									"001010100", "100010001" ].map(function(x){
+																	return parseInt(x, 2);
+																}),
 				currentState 	= gameStateArray.map(function(x){
 					 if(x.getCurrentTile() === currentPlayer){
 					 	return 1;
@@ -142,12 +204,12 @@ function gameboard(){
 						return currentPlayer + " wins!";
 					}
 				}
-				if(this.getLegalMoves(gameStateArray).length === 0){
+				if(this.board.getLegalMoves(gameStateArray).length === 0){
 					return "Tie game!";					
 				}
 		},
 		getLegalMoves: function(gameStateArray){
-			var tmpArray = []
+			var tmpArray = [];
 			gameStateArray.map(function(x, idx){
 				if(!x.hasData()){
 					tmpArray.push(idx);				
@@ -155,116 +217,5 @@ function gameboard(){
 			});
 			return tmpArray;
 		}
-	}
-}
-
-function aiPlayer(boardObj){
-	return {
-		gameboard 	: boardObj,
-		depth		: this.gameboard.depth,
-		playerTile	: this.gameboard.playerTile,
-		aiTile		: this.gameboard.aiTile,
-
-		minimax		: function(depth, player){
-						//console.log(this);
-						var tmpTiles		= this.gameboard.tiles,
-							legalMoves		= this.gameboard.getLegalMoves(tmpTiles),
-							bestMove		= -1,
-							best			= (player === this.playerTile ? -1000 : 1000);
-
-							if(legalMoves.length === 0 || depth === 0){
-								best = evaluate();
-							} else {
-								for(var i = legalMoves.length; i--;){
-									var move = legalMoves[i];
-									tmpTiles[move].setCurrentTile(player);
-									if(player === this.aiTile){
-										
-										var current = this.mimimax(depth - 1, player);
-											current = current[0];
-										if(current > best){
-											best = current;
-											bestMove = move;
-										}
-									} else {
-										var current = this.minimax(depth - 1, this.aiTile);
-											current = current[0];
-										if(current < best){
-											best = current;
-											bestMove = move;
-										}
-									}
-								}
-							}
-
-							return [best, bestMove];
-					},
-		evaluate	: function(){
-			const moves = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-			var score = 0;
-			for(var i = moves.length; i--;){
-				score += evaluateLine(moves[i]);
-			}
-			return score;
-		},
-		evaluateLine: function(arr){
-			var score = 0;
-			if(this.tmpTiles[arr[0]].equals(this.aiTile)){
-				score = 1;
-			} else if(this.tmpTiles[arr[0]].equals(this.playerTile)){
-				score = -1;
-			}
-
-			if(this.tmpTiles[arr[1]].equals(this.aiTile)){
-				if(score === 1){
-					score = 10;
-				} else if(score === -1){
-					return 0;
-				} else {
-					score = 1;
-				}
-
-			} else if(this.tmpTiles[arr[1]].equals(this.playerTile)){
-				if(score === -1){
-					score = -10;
-				} else if(score === 1){
-					return 0;
-				} else {
-					score = -1;
-				}
-			}
-
-			if(this.tmpTiles[arr[2]].equals(this.aiTile)){
-				if(score > 0){
-					score *= 10;
-				} else if(score < 0){
-					return 0;
-				} else {
-					score = 1;
-				}
-			} else if (this.tmpTiles[arr[2]].equals(this.playerTile)){
-				if(score < 0){
-					score *= 10;
-				} else if(score > 0){
-					return 0;
-				} else {
-					score = -1;
-				}
-			}
-
-			return score;
-		}
-
-	}
-}
-
-function GameController(){
-	return {
-		gameMode		:	null,
-		difficulty		:	0, //Will be a number used as depth for minimax
-		currentPlayer	:	null,
-		playerTile		:	null,
-		aiTile			:	null,
-
 	}
 }
